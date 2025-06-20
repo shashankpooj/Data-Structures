8c0c1c33b29eb014b08f973d7adcb44f2f84800b
Shashank Poojary <138311524+shashankpooj@users.noreply.github.com>
2025-06-20 16:23:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (77.16%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,14 +5,10 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # for i in range(len(nums)):
-        #     for j in range(i+1,len(nums)):
-        #         if nums[i]+nums[j]==target:
-        #             return [i,j]    0
-
-        comp={}
-        for i in range(len(nums)):
-            if nums[i] in comp:
-                return [i,comp[nums[i]]]
-            else:
-                comp[target-nums[i]]=i
+        prevMap={} #val:index
+        for i,n in enumerate(nums):
+            diff=target-n
+            if diff in prevMap:
+                return [prevMap[diff],i]
+            prevMap[n]=i
+        return
