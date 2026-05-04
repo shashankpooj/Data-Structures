9a36dd0cd74213db54ae2e1d107ffb3a983f59e3
Shashank Poojary <138311524+shashankpooj@users.noreply.github.com>
2026-05-04 23:40:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (99.18%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -6,10 +6,12 @@ class Solution(object):
         """
         i=0
         j=i+1
-        while j<len(nums):
+        n=len(nums)-1
+        while j<=n:
             if nums[i]!=nums[j]:
-                nums[i+1]=nums[j]
                 i=i+1
+                
+                nums[i]=nums[j]
                 j=j+1
                 # continue
             else:
