ace7eaed0a8d4bbc58f42acc895582ed28156c2d
Shashank Poojary <138311524+shashankpooj@users.noreply.github.com>
2026-05-04 23:57:43 +0530

    Time: 15 ms (45.20%) | Memory: 14.7 MB (9.31%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -4,32 +4,38 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        nums1=[]
-        nums2=[]
+        arr1=[]
+        arr2=[]
+        for i in range(len(nums)):
+            if nums[i]<=0:
+                arr1.append(nums[i]**2)
+            else:
+                arr2.append(nums[i]**2)
+        arr1.reverse()
+        n1=len(arr1)
+        n2=len(arr2)
         i=0
         j=0
         res=[]
-
-        for k in range(0,len(nums)):
-            if nums[k]<0:
-                nums1.append(nums[k]**2)
-            else:
-                nums2.append(nums[k]**2)
-        nums1.reverse()
-        while i<len(nums1) and j<len(nums2):
-            if nums1[i]<nums2[j]:
-                res.append(nums1[i])
+        while i<n1 and j<n2:
+            if arr1[i]<arr2[j]:
+                res.append(arr1[i])
                 i=i+1
             else:
-                res.append(nums2[j])
+                res.append(arr2[j])
                 j=j+1
-        while j<len(nums2):
-            res.append(nums2[j])
-            j=j+1
-        while i<len(nums1):
-            res.append(nums1[i])
+        while i<n1:
+            res.append(arr1[i])
             i=i+1
+        while j<n2:
+            res.append(arr2[j])
+            j=j+1
         return res
+        
+
+
+        
+
 
 
 
