98fc31dda5aeea083017b294b0754705fa231c01
Shashank Poojary <138311524+shashankpooj@users.noreply.github.com>
2026-05-05 17:18:51 +0530

    Time: 24 ms (9.47%) | Memory: 14.4 MB (9.40%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -6,16 +6,16 @@ class Solution(object):
         """
         arr1=[]
         arr2=[]
-        for i in range(len(nums)):
-            if nums[i]<=0:
-                arr1.append(nums[i]**2)
+        for k in range(len(nums)):
+            if nums[k]<=0:
+                arr1.append(nums[k]**2)
             else:
-                arr2.append(nums[i]**2)
+                arr2.append(nums[k]**2)
         arr1.reverse()
-        n1=len(arr1)
-        n2=len(arr2)
         i=0
         j=0
+        n1=len(arr1)
+        n2=len(arr2)
         res=[]
         while i<n1 and j<n2:
             if arr1[i]<arr2[j]:
@@ -24,20 +24,16 @@ class Solution(object):
             else:
                 res.append(arr2[j])
                 j=j+1
-        while i<n1:
+        while  i<n1:
             res.append(arr1[i])
             i=i+1
-        while j<n2:
+        while  j<n2:
             res.append(arr2[j])
             j=j+1
         return res
         
-
-
         
 
-
-
-
+            
 
         
