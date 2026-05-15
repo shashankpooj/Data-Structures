48ddbbb322eb842ba198dd592dba8e581f646182
shashank-poojary-simpleenergy <138311524+shashank-poojary-simpleenergy@users.noreply.github.com>
2026-05-15 22:40:58 +0530

    Time: 27 ms (6.84%) | Memory: 14.6 MB (8.91%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -4,36 +4,35 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        arr1=[]
-        arr2=[]
-        for k in range(len(nums)):
-            if nums[k]<=0:
-                arr1.append(nums[k]**2)
-            else:
-                arr2.append(nums[k]**2)
-        arr1.reverse()
+        nums1=[]
+        nums2=[]
         i=0
         j=0
-        n1=len(arr1)
-        n2=len(arr2)
         res=[]
-        while i<n1 and j<n2:
-            if arr1[i]<arr2[j]:
-                res.append(arr1[i])
-                i=i+1
-            else:
-                res.append(arr2[j])
-                j=j+1
-        while  i<n1:
-            res.append(arr1[i])
-            i=i+1
-        while  j<n2:
-            res.append(arr2[j])
-            j=j+1
-        return res
-        
         
 
-            
+        for k in range(0,len(nums)):
+            if nums[k]<0:
+                nums1.append(nums[k]**2)
+            else:
+                nums2.append(nums[k]**2)
+        nums1.reverse()
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]<nums2[j]:
+                res.append(nums1[i])
+                i=i+1
+            else:
+                res.append(nums2[j])
+                j=j+1
+        while j<len(nums2):
+            res.append(nums2[j])
+            j=j+1
+        while i<len(nums1):
+            res.append(nums1[i])
+            i=i+1
+        return res
+
+
+
 
         
