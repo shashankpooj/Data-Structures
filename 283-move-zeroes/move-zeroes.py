5775c3c0c824a5c9cdbd35987e830524b68a66b0
shashank-poojary-simpleenergy <138311524+shashank-poojary-simpleenergy@users.noreply.github.com>
2026-05-17 09:19:06 +0530

    Time: 3 ms (90.03%) | Memory: 13.5 MB (76.70%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,17 +1,20 @@
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        
-        left = 0
-        right = 0
-
-        while right < len(nums):
-            if nums[right] != 0:
-                nums[left], nums[right] = nums[right], nums[left]
-                left += 1
-            right += 1
-
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        left=0
+        for right in range(len(nums)):
+            if nums[right]!=0:
+                nums[left],nums[right]=nums[right],nums[left]
+                left+=1
         return nums
+            
+
+     
+            
 
 
-        
+
         
